60c89c6ec6564104e863be44d25abc1e0fc52494
OlgaRedozubova <olgared82@gmail.com>
2020-02-19 14:11:49 +0200

    TEst

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ losswise.set_api_key('DJOOF0FUN')
 session = losswise.Session(tag='my_dilated_convnet', max_iter=10,
                            params={'cnn_size': 20})
 graph = session.graph('loss', kind='min')
-session.set_values({ 'key5': 'eee'})
+session.set_values({ 'key5': 'eee1'})
 for x in range(10):
     train_loss = 1. / (0.1 + x + 0.1 * random.random())
     test_loss = 1.5 / (0.1 + x + 0.2 * random.random())
